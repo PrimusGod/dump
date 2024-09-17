380db79ed8e6ac3d92b2efbb2b693a3d067230e2
MTK <MTK@MTK.com>
2024-09-17 05:06:35 -0700

    Initial commit of sofiap

---
+++ BoardConfig.mk
@@ -0,0 +1,22 @@
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
+
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/Motorola/sofiap/kernel
+
+BOARD_MKBOOTIMG_ARGS := --base 00000000 --pagesize 4096 --kernel_offset 00008000 --ramdisk_offset 01000000 --tags_offset 00000100   --cmdline 'console=ttyMSM0,115200,n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.usbcontroller=4e00000.dwc3 printk.devkmsg=on androidboot.hab.csv=29 androidboot.hab.product=sofiap androidboot.hab.cid=21 firmware_class.path=/vendor/firmware_mnt/image buildvariant=user'  --dt ~/omni/device/Motorola/sofiap/boot.img-dtb
+
+#MTK common twrp flags
+TW_NO_EXFAT := true
+TWHAVE_SELINUX := true
+#TW_USE_TOOLBOX := true
+TW_NO_EXFAT_FUSE := true
+TW_THEME := portrait_mdpi
+#RECOVERY_SDCARD_ON_DATA := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
+BOARD_CUSTOM_BOOTIMG_MK := device/Motorola/sofiap/boot.mk
